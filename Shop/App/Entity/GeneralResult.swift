f17824cb7e1ee7f105979d4d71638c19c1bd6ed0
Artem <artem.fx4y@gmail.com>
2021-08-01 15:33:13 +0300

    feature(network): add user authentication

---
+++ Shop/App/Entity/GeneralResult.swift
@@ -0,0 +1,14 @@
+//
+//  GeneralResult.swift
+//  Shop
+//
+//  Created by MacBook Pro on 31.07.2021.
+//
+
+import Foundation
+
+struct GeneralResult: Codable {
+
+	let result: Int
+	let userMessage: String?
+}
